833473db7ea888cbcddbc800df1649d8d1e5a281
GITjaiprakas <107695687+GITjaiprakas@users.noreply.github.com>
2022-09-24 21:22:46 +0530

    Update 9Q.c

--- 9Q.c
+++ 9Q.c
@@ -1,26 +1,16 @@
+
 #include<stdio.h>
     int main(){
-        int n1,n2,i;
+        int n1,n2,i,j,k;
         printf("Enter first number and second number : ");
         scanf("%d %d",&n1,&n2);
-        if(n1>n2){
-            for(i=1;i<=n2;i++){
-                if(n1*i%n2==0){
-                    break;
-                }
-            } 
-            printf("lcm of %d and %d is %d",n1,n2,n1*i);   
-        }
-        else if(n2>n1){
-            for(i=1;i<=n1;i++){
-                if(n2*i%n1==0){   
-                    break;
-                }
+        i=n1>n2?n1:n2;
+        j=n1<n2?n1:n2;
+        for(k=1;k<=j;k++){
+            if(i*k%j==0){
+                break;
             }
-            printf("lcm of %d and %d is %d",n1,n2,n2*i);
-        }
-        else{
-            printf("The lcm of %d and %d is %d",n1,n2,n1);
-        }
+        } 
+        printf("lcm of %d and %d is %d",n1,n2,i*k);           
         return 0;
     }
